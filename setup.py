0fc125cee382e0c343d2cdb6a1eda7aa05479fd1
Pierson Browne <pbrowne88@gmail.com>
2021-01-19 13:34:31 -0500

    added a necessary __init__.py that wasn't required in editable mode

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pdpp",
-    version="0.3.9",
+    version="0.4.1",
     install_requires=['Click>=7.0', 'doit>=0.31.1', 'networkx>=2.2', 'graphviz>=0.10.1', 'pydot>=1.4.1', 'questionary>=1.0.2', 'pyyaml>=5.3.1'],
     packages=find_packages(),
 	include_package_data=True,
@@ -12,13 +12,13 @@ setup(
         """,
 
     author="Pierson Browne, Rachel Wood, Tyler Crick, John McLevey",
-    author_email="pbrowne@uwaterloo.ca",
+    author_email="pbrowne88@gmail.com",
     description="Command line tool for automation, transparency, and reproducibility in data processing projects",
     keywords="doit",
     url="http://networkslab.org",
-	classifiers=(
+	classifiers=[
 		"Programming Language :: Python :: 3",
 		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
-	),		
+    ],		
 )
